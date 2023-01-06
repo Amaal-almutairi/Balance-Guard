0ab83cba812dcc741d37d7fdf0ea64f2cf574794
amaal <amaal772@gmail.com>
2023-01-06 09:26:25 +0300

    Initial Commit

---
+++ Balance-Guard/Balance_GuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Balance_GuardApp.swift
+//  Balance-Guard
+//
+//  Created by Amaal Almutairi on 13/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Balance_GuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
